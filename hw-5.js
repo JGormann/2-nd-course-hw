e9b5822012277f9bc526f643b6b3b1fb2475c6c2
DESKTOP-80BSOKE <gormanjoseph23@gmail.com>
2024-02-19 12:36:20 +0500

    Сделал правки в задания 3.1, 4 и 6 и добавил 7е задание

--- hw-5.js
+++ hw-5.js
@@ -21,9 +21,9 @@ function calc(n) {
 //Задание 3
 //3.1
 function num(a) {
-  return a**2
+  console.log(a**2);
 }
-console.log(num(6));
+num(6)
 
 //3.2
 function num(a) {
@@ -34,44 +34,68 @@ alert (result)
 
 //Задание 4
 "use strict";
-let age = prompt("Сколько Вам лет?");
-
-let printMessage;
-
-if (age < 0) {
-    printMessage = function () {
+function control(){
+  let age = prompt("Сколько Вам лет?");
+  if (age <= 0) {
         console.log("Вы ввели неправильное значение");
-    }
-} else if (age < 12) {
-    printMessage = function () {
-        console.log("Привет, друг!");
-    }
-} else {
-    printMessage = function () {
+} else if (age < 12) { 
+        console.log("Привет, друг!");    
+} else if (age > 12) { 
         console.log("Добро пожаловать!");
-    }
+}else if (isNaN(age)){
+      console.log("Вы ввели неверное значение");
+}else if (age === null || age === ""){
+  console.log("Вы не ответили на вопрос!");
 }
-
-printMessage();
+}
+control()
 
 //Задание 5
 function check(num1, num2) {
-  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+  if (isNaN(num1) || isNaN(num2)) {
     return 'Одно или оба значения не являются числом';
   } else {
     return num1 * num2;
   }
 }
-console.log(check (2, 6)); 
+console.log(check ('7w', 6)); 
 
 //Задание 6
 let num = prompt("Введите ваше число");
 function calc(num) {
   if(isNaN(num)){
     console.log("Переданный параметр не является числом");
+  }else if (num === null || num === ""){
+    console.log("Вы не ответили на вопрос!");
   }else{
     return  num ** 3;
   }
 }
 
 console.log(calc(num));
+
+// Задание 7
+function getArea() {
+    return 3.14 * this.radius * this.radius;
+   }
+  
+function getPerimeter() {
+     return 2 * 3.14 * this.radius;
+   }
+  
+const circle1 = {
+     radius: 5,  
+    getArea: getArea,   
+    getPerimeter: getPerimeter,
+   };
+  
+const circle2 = {
+     radius: 10,  
+     getArea,     
+     getPerimeter, 
+   };
+  
+   console.log(circle1.getArea());
+   console.log(circle2.getArea());
+   console.log(circle1.getPerimeter());
+   console.log(circle2.getPerimeter());
